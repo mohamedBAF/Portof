f7b6443a59ca145ed8aa70ac27a59b4f4e3770f7
mohamedBAF <mohamed.benabedelfattah@esprit.tn>
2020-11-11 15:32:02 +0100

    Updates

---
+++ precache-manifest.11be6f4e4f2343a84f00fdba8a1c0d51.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35868a06cae4d6d76d50565f91d90d73",
+    "url": "/Portof/index.html"
+  },
+  {
+    "revision": "00f095c9ac2adf601d7b",
+    "url": "/Portof/static/css/2.d2ca2446.chunk.css"
+  },
+  {
+    "revision": "27725bf95f87878d81b2",
+    "url": "/Portof/static/css/main.bd403592.chunk.css"
+  },
+  {
+    "revision": "00f095c9ac2adf601d7b",
+    "url": "/Portof/static/js/2.6110ae6d.chunk.js"
+  },
+  {
+    "revision": "27725bf95f87878d81b2",
+    "url": "/Portof/static/js/main.c719e2c1.chunk.js"
+  },
+  {
+    "revision": "953af86b3fe1e24bc03a",
+    "url": "/Portof/static/js/runtime-main.cfa28a7d.js"
+  },
+  {
+    "revision": "a599d80f0e171a30eb1432e14e44e07d",
+    "url": "/Portof/static/media/Cvv.a599d80f.png"
+  },
+  {
+    "revision": "5c653daace009c6d0d9c4fd8f7f265ff",
+    "url": "/Portof/static/media/cv.5c653daa.png"
+  },
+  {
+    "revision": "4c1cba5f7496cf94240aa8aa3d67f76a",
+    "url": "/Portof/static/media/cv2.4c1cba5f.png"
+  },
+  {
+    "revision": "2da239b51f31c877b33bb3bf4e81c849",
+    "url": "/Portof/static/media/cv3.2da239b5.png"
+  },
+  {
+    "revision": "fdf35f0af93d30d092cb284563dbb732",
+    "url": "/Portof/static/media/cv4.fdf35f0a.png"
+  },
+  {
+    "revision": "0a04cd22a69442e31c3df8f8e1adff1b",
+    "url": "/Portof/static/media/foad_logo.0a04cd22.jpg"
+  },
+  {
+    "revision": "c31b5069f157b82943809ab2ecc38ef8",
+    "url": "/Portof/static/media/git.c31b5069.svg"
+  },
+  {
+    "revision": "f20b8deae88deea2e0dedf1b9982cafc",
+    "url": "/Portof/static/media/photoeffets.com.f20b8dea.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Portof/precache-manifest.11be6f4e4f2343a84f00fdba8a1c0d51.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portof/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
